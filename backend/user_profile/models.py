fc2309ee3766a78e8d7c300901d5340515c9c486
vinyas-bharadwaj <vinyasbharadwaj101@gmail.com>
2025-04-11 16:21:14 +0530

    added image identification and generation endpoint

--- backend/user_profile/models.py
+++ backend/user_profile/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    name = models.CharField(max_length=100)
+    contact = models.CharField(max_length=20)
+    city = models.CharField(max_length=100)
+    age = models.PositiveIntegerField()
+    occupation = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return f"{self.name}'s Profile"
+
+# Signal to create a profile when a user is created
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
+
--- backend/user_profile/views.py
+++ backend/user_profile/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Profile
+from .serializers import ProfileSerializer
 
 # Create your views here.
+class ProfileViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows profiles to be viewed, created, edited, and deleted.
+    """
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
